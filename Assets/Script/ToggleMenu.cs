4c819f2e89f7ff1f1503c2929757938791334282
Dracobastien55 <dracobastien55@gmail.com>
2021-01-13 22:48:27 +0100

    Add ToggleMenu
    ToggleMenu don't work

---
+++ Assets/Script/ToggleMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToggleMenu : MonoBehaviour
+{
+    public Toggle toggle;
+    public GameObject background;
+
+    public void ToggleValueChanged()
+    {
+        toggle = GetComponent<Toggle>();
+        if (toggle.isOn) background.GetComponentInParent<Renderer>().enabled = false ;
+        else background.GetComponentInParent<Renderer>().enabled = true;
+    }
+}
