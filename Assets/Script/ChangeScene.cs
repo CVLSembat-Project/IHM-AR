e240807cbaa7233b264802ed528bb4c90b178520
Dracobastien55 <dracobastien55@gmail.com>
2021-01-16 16:38:51 +0100

    Add Scene + Class to change Scene
    Add Scene and method for changing scene when we click on a button

---
+++ Assets/Script/ChangeScene.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+
+    private AssetBundle loadedScene;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        loadedScene = AssetBundle.LoadFromFile("Assets/Scenes");
+    }
+
+    public void LoadingScene(string SceneName)
+    {
+        SceneName = "Visualisation";
+        SceneManager.LoadScene(SceneName, LoadSceneMode.Single);
+    }
+}
