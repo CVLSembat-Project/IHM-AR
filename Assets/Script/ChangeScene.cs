8efac123de9abc8f04e27cf6ae69f1271602dbdd
Dracobastien55 <dracobastien55@gmail.com>
2021-01-17 18:19:56 +0100

    Method to change Scene when click on button + branch to Merge
    With SceneMananer in LoadSceneMode single to load only one scene and open it.

--- Assets/Script/ChangeScene.cs
+++ Assets/Script/ChangeScene.cs
@@ -5,19 +5,10 @@ using UnityEngine.SceneManagement;
 
 public class ChangeScene : MonoBehaviour
 {
-
-    private AssetBundle loadedScene;
     
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        loadedScene = AssetBundle.LoadFromFile("Assets/Scenes");
-    }
-
     public void LoadingScene(string SceneName)
     {
-        SceneName = "Visualisation";
-        SceneManager.LoadScene(SceneName, LoadSceneMode.Single);
+        //We get in parameter the Scene name to load it and show it.
+        SceneManager.LoadScene(SceneName);
     }
 }
