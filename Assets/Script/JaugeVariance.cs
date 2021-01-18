683d27ce9a6e6dc56f28feed35101ea81f221b3a
Dracobastien55 <dracobastien55@gmail.com>
2021-01-18 16:50:59 +0100

    Jauge + Code
    Add database + Jauge

---
+++ Assets/Script/JaugeVariance.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JaugeVariance : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        //TODO : Add request HTTP to get value of database
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localScale += new Vector3(0,-5,0) * Time.deltaTime;
+    }
+}
