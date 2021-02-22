deb59a14414cb91adef4c70dbfa60b08c20c0785
Dracobastien55 <dracobastien55@gmail.com>
2021-02-22 17:56:35 +0100

    Positionning very good
    Possitionning check and web request to try

---
+++ Assets/Script/WebRequest.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Networking;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WebRequest : MonoBehaviour
+{
+    public Text text;
+    // Update is called once per frame
+    void Update()
+    {
+        print("début de séance de requête");
+        StartCoroutine(GetRequest("http://172.19.127.23/Batiment/A/gaz"));
+    }
+
+    IEnumerator GetRequest(string uri)
+    {
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(uri))
+        {
+            yield return webRequest.SendWebRequest();
+
+            string[] pages = uri.Split('/');
+            int page = pages.Length - 1;
+
+            if (webRequest.isNetworkError)
+            {
+                Debug.Log(pages[page] + ": Error" + webRequest.error);
+            }
+            else
+            {
+                Debug.Log(pages[page] + ":\nReceived : " + webRequest.downloadHandler.text);
+                text.text = webRequest.downloadHandler.text;
+            }
+        }
+    }
+}
